3d7d2008c617ba04b3e97be033236e744ea8194f
xiaotao <613961636@qq.com>
2017-12-26 17:49:07 +0800

    random unikey

--- xServer/game-server/app/servers/zgate/handlers/CSHeartBeat.js
+++ xServer/game-server/app/servers/zgate/handlers/CSHeartBeat.js
@@ -1,5 +1,6 @@
 var logger = require('pomelo-logger').getLogger('pomelo');
 var MsgProtobuf = require('../../../../modules/MsgProtobuf');
+var Random = require('../../../common/Random');
 
 var CSHeartBeat = function () {
 }
@@ -10,12 +11,12 @@ CSHeartBeat.prototype.execute = function (connection, buff) {
     logger.debug("receive buff:"+ JSON.stringify(receive_data));
 	let sndData = {
     	msgid : protoNameSpace.MsgID.SysProto_HeartBeat,
-    	time : 2,
-        serverinfo : "serverinfo"
+    	time : Date.now(),
+        serverinfo : Random.genUniqKey()
     }
     let protoMsg = protoNameSpace.HeartBeat.create(sndData);
     let __bytes = protoNameSpace.HeartBeat.encode(protoMsg).finish();
-    //connection.sendMessage(protoMsg.msgid, __bytes);
+    connection.sendMessage(protoMsg.msgid, __bytes);
 }
 
 module.exports = CSHeartBeat;
